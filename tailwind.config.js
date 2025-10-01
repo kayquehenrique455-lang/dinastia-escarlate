6a71b0527ea39cfeec39aa2f6202842f3ce910a2
kayquehenrique455-lang <kayquehenrique455@gmail.com>
2025-10-01 09:48:24 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
-
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        scarlet: '#6b0f0f',
+        gold: '#ffd700',
+        darkbg: '#0b0909'
+      },
+      fontFamily: {
+        display: ['"Cinzel"', 'serif'],
+      }
+    },
   },
   plugins: [],
 }
